01831fe4249e168f1328382ed5d9f03638b43ca2
bentoluizv <bentoluizv@gmail.com>
2024-05-07 21:53:46 -0300

    remove unused files

--- app/service/accommodation_service.py
+++
@@ -1,6 +0,0 @@
-"""The service layer implements the application's business rules"""
-
-class AccommodationService:
-    """represents the rules related to the accommodation entity"""
-
-# TODO implementar as funções que garantem os requisitos levantados relacionados com a entidade acomodação e garantir que todas as regras sejam aplicadas.
