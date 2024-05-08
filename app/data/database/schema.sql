138ee03dc223fdf5bd6cb2147853f7eb65eb2bb8
bentoluizv <bentoluizv@gmail.com>
2024-05-07 21:25:29 -0300

    changes in schema sql

--- app/data/database/schema.sql
+++ app/data/database/schema.sql
@@ -1,39 +1,28 @@
 CREATE TABLE IF NOT EXISTS guest (
-    uuid                    BLOB PRIMARY KEY,
-    created_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    document,               TEXT PRIMARY KEY,
+    created_at              TEXT NOT NULL,
     name                    TEXT NOT NULL,
     surname                 TEXT NOT NULL,
     country                 TEXT NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS phones_per_guest (
+    id                      INTEGER PRIMARY KEY AUTOINCREMENT,
+    document                TEXT NOT NULL,
+    phone                   TEXT NOT NULL,
+    FOREIGN KEY(document) REFERENCES guest(document)
+);
+
 CREATE TABLE IF NOT EXISTS accommodation (
-    uuid                    BLOB PRIMARY KEY,
-    created_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    uuid                    TEXT PRIMARY KEY,
+    created_at              TEXT NOT NULL,
     status                  TEXT NOT NULL,
     name                    TEXT NOT NULL,
     total_guests            INTEGER NOT NULL,
     single_beds             INTEGER NOT NULL,
     double_beds             INTEGER NOT NULL,
-    min_nights              INTEGER NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS booking (
-    uuid                    BLOB PRIMARY KEY,
-    created_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    status                  TEXT NOT NULL,
-    check_in                TIMESTAMP NOT NULL,
-    check_out               TIMESTAMP NOT NULL,
-    guest_uuid              BLOB NOT NULL,
-    accommodation_uuid      BLOB NOT NULL,
-    FOREIGN KEY(guest_uuid) REFERENCES guest(uuid),
-    FOREIGN KEY(accommodation_uuid) REFERENCES accommodation(uuid)
-);
-
-CREATE TABLE IF NOT EXISTS booking_with_special_request (
-    id                      INTEGER PRIMARY KEY AUTOINCREMENT,
-    booking_uuid            BLOB NOT NULL,
-    special_request         VARCHAR(500) NOT NULL,
-    FOREIGN KEY(booking_uuid) REFERENCES booking(uuid)
+    min_nights              INTEGER NOT NULL,
+    price                   INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS amenities (
@@ -43,16 +32,29 @@ CREATE TABLE IF NOT EXISTS amenities (
 
 CREATE TABLE IF NOT EXISTS amenities_per_accommodation (
     id                      INTEGER PRIMARY KEY AUTOINCREMENT,
-    accommodation_uuid      BLOB NOT NULL,
+    accommodation_uuid      TEXT NOT NULL,
     amenitie_id             INTEGER NOT NULL,
     FOREIGN KEY(accommodation_uuid) REFERENCES accommodation(uuid),
     FOREIGN KEY(amenitie_id) REFERENCES amenities(id)
 );
 
-CREATE TABLE IF NOT EXISTS phones_per_guest (
+
+CREATE TABLE IF NOT EXISTS booking (
+    uuid                    TEXT PRIMARY KEY,
+    created_at              TEXT NOT NULL,
+    status                  TEXT NOT NULL,
+    check_in                TEXT NOT NULL,
+    check_out               TEXT NOT NULL,
+    document              TEXT NOT NULL,
+    accommodation_uuid      TEXT NOT NULL,
+    FOREIGN KEY(document) REFERENCES guest(uuid),
+    FOREIGN KEY(accommodation_uuid) REFERENCES accommodation(uuid)
+);
+
+CREATE TABLE IF NOT EXISTS requests_per_bookings (
     id                      INTEGER PRIMARY KEY AUTOINCREMENT,
-    guest_uuid              BLOB NOT NULL,
-    phone                   TEXT NOT NULL,
-    FOREIGN KEY(guest_uuid) REFERENCES guest(uuid)
+    booking_uuid            TEXT NOT NULL,
+    special_request         TEXT NOT NULL,
+    FOREIGN KEY(booking_uuid) REFERENCES booking(uuid)
 );
 
