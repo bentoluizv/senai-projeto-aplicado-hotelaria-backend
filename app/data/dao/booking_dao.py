15912f82248b2b088bf173babaf6ca4dc2049167
bentoluizv <bentoluizv@gmail.com>
2024-05-07 21:24:39 -0300

    impl booking dao count func

--- app/data/dao/booking_dao.py
+++ app/data/dao/booking_dao.py
@@ -3,7 +3,6 @@
 """Data Access for Booking"""
 
 from sqlite3 import Connection
-from typing import Any
 
 
 class BookingDAO:
@@ -11,9 +10,20 @@ class BookingDAO:
 
     def __init__(self, db: Connection):
         self.db = db
-        self.data = {}
 
-    def find(self, params: dict[str, str]):
+    def count(self):
+        """counts all records"""
+        cursor = self.db.cursor()
+        cursor.execute('SELECT COUNT(*) FROM booking')
+        cursor.close()
+
+    def insert(self, params):
+        """creates a new register"""
+        cursor = self.db.cursor()
+        cursor.execute('INSERT INTO booking (uuid, status, check_in, check_out, guest_uuid, accommodation_uuid) VALUES (:uuid, :status, :check_in, :check_out :guest_uuid :accommodation_uuid;)', params)
+        cursor.close()
+
+    def find(self, params):
         """find searches for a value according to its uuid, params -> { uuid: str }"""
         cursor = self.db.cursor()
         cursor.execute('SELECT * FROM booking JOIN guest ON booking.guest_uuid = guest.uuid JOIN accommodation ON booking.accommodation_uuid = accommodation.uuid WHERE booking.uuid = :uuid;', params)
@@ -29,13 +39,13 @@ class BookingDAO:
         cursor.close()
         return res
 
-    def update(self, params: dict[str, Any]) -> None:
+    def update(self, params):
         """updates a record in the table as a whole. params must have the uuid of the data to be updated as well as all the entity values in the table"""
         cursor = self.db.cursor()
         cursor.execute('UPDATE booking SET status = :status, name = :name, total_guests = :total_guests, single_beds = :single_beds, double_beds = :double_beds, min_nights = :min_nights WHERE uuid = :uuid', params)
         cursor.close()
 
-    def delete(self, params: dict[str, str]):
+    def delete(self, params):
         """delete a record based on its uuid, params -> { uuid: str }"""
         cursor = self.db.cursor()
         cursor.execute('DELETE FROM booking WHERE uuid = :uuid', params)
