232339439f40485db9a48c8bcd77ec15581f2d78
bentoluizv <bentoluizv@gmail.com>
2024-05-16 12:48:56 -0300

    guests do testing suite

--- tests/integration/test_dao_guests.py
+++ tests/integration/test_dao_guests.py
@@ -1,9 +1,7 @@
-from datetime import datetime
-
 from pytest import fixture
 from app.data.dao.guest_dao import GuestDAO
 from app.data.database.db import get_db
-from app.domain.Guests import Guest
+from app.domain.Guests import Guest, GuestDTO
 
 @fixture
 def guest_dao(app):
@@ -17,29 +15,49 @@ def test_guest_dao_count(guest_dao):
 
 
 def test_guest_dao_insert(guest_dao):
-    guest = Guest('03093331056','Ana Claudia', 'Costa', 'Brazil', '4832395853')
+    guest_dto: GuestDTO = {
+        'document':'03093331056',
+        'name':'Ana Claudia',
+        'surname':'Costa',
+        'country':'Brazil',
+        'phone':'4832395853',
+        'created_at': None
+        }
+    guest = Guest(guest_dto)
     guest_dao.insert(guest)
     assert guest_dao.count() == 5
 
 
 def test_guest_dao_select(guest_dao):
-    guest = guest_dao.select("00157624242")
+    guest = guest_dao.find("00157624242")
     assert guest.name == 'Bento Luiz'
 
 
 def test_guest_dao_select_many(guest_dao):
-    res = guest_dao.select_many()
+    res = guest_dao.find_many()
     assert len(res) == 4
 
 def test_guest_dao_update(guest_dao):
-    exists = guest_dao.select('00157624242')
+    exists = guest_dao.find('00157624242')
     assert exists is not None
-    guest = Guest("00157624242", 'Bento Luiz', 'V M da S Neto', 'Brazil', '4832395853', exists.created_at)
+
+    guest_dto: GuestDTO = {
+        'document': "00157624242",
+        'name': 'Bento Luiz',
+        'surname': 'V M da S Neto',
+        'country': 'Brazil',
+        'phone':' 4832395853',
+        'created_at': exists.created_at
+    }
+
+    guest = Guest(guest_dto)
     guest_dao.update(guest)
-    assert guest_dao.select('00157624242').surname == 'V M da S Neto'
+    result = guest_dao.find("00157624242")
+    assert result.surname == 'V M da S Neto'
+
 
 
 def test_guest_dao_delete(guest_dao):
     guest_dao.delete("00157624242")
-    res = guest_dao.select("00157624242")
+    res = guest_dao.find("00157624242")
     assert res is None
