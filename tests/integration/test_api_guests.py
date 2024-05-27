201fcb0589b3cd5daf640805ac20f746d76bcd05
bentoluizv <bentoluizv@gmail.com>
2024-05-27 09:40:21 -0300

    Fixed update tests, json headers set correctly.

--- tests/integration/test_api_guests.py
+++ tests/integration/test_api_guests.py
@@ -1,27 +1,24 @@
 import json
 
-import pytest
-
-
 from app.entity.Guests import GuestDTO
 
 
 def test_api_get_guests(client):
-    response = client.get('/hospedes/')
+    response = client.get('/api/hospedes')
     guests = json.loads(response.data)
     assert len(guests) == 4
     assert response.status_code == 200
 
 
 def test_api_get_guests_id(client):
-    response = client.get('/hospedes/00157624242')
+    response = client.get('/api/hospedes/00157624242')
     guests = json.loads(response.data)
     assert guests['name'] == 'Bento Luiz'
     assert response.status_code == 200
 
 
 def test_api_get_guests_id_not_found(client):
-    response = client.get('/hospedes/00157624')
+    response = client.get('/api/hospedes/00157624')
     assert response.status_code == 404
 
 
@@ -35,25 +32,26 @@ def test_api_post_guests(client):
         'created_at': None
         }
 
-    response = client.post('/hospedes/cadastro', data=guest_dto)
+    response = client.post('/api/hospedes/cadastro', data=guest_dto)
 
     assert response.status_code == 201
     assert response.text == 'CREATED'
 
 
 def test_api_post_guests_bad_request(client):
-    response = client.post('/hospedes/cadastro', data=None)
+    response = client.post('/api/hospedes/cadastro', data=None)
     assert response.status_code == 400
 
 
+
 def test_api_delete_guests(client):
-    response = client.delete('/hospedes/00157624242')
+    response = client.delete('/api/hospedes/00157624242')
     assert response.status_code == 200
     assert response.text == 'DELETED'
 
 
 def test_api_delete_guests_not_found(client):
-    response = client.delete('/hospedes/0015242')
+    response = client.delete('/api/hospedes/0015242')
     assert response.status_code == 404
 
 
@@ -66,8 +64,14 @@ def test_api_updated_guests(client):
         'phone': '48992054211',
         'created_at': None
     }
-    response = client.put('/hospedes/', data=data)
-    assert response.status_code == 200
+
+    response = client.put(
+        '/api/hospedes',
+        data=json.dumps(data),
+        headers={'content-type': 'application/json'}
+        )
+
+    assert response.status_code == 201
     assert response.text == 'UPDATED'
 
 
@@ -80,18 +84,10 @@ def test_api_updated_guests_not_found(client):
         'phone': '48992054211',
         'created_at': None
     }
-    response = client.put('/hospedes/', data=data)
+    response = client.put(
+        '/api/hospedes',
+        data=json.dumps(data),
+        headers={'content-type': 'application/json'}
+        )
+
     assert response.status_code == 404
-
-
-
-def test_api_updated_guests_bad_request(client):
-    data = {
-        'document': '',
-        'name': 'Bento Luiz',
-        'surname': 'V M da S Neto',
-        'country': 'Brazil',
-        'phone': '489920542115432'
-    }
-    response = client.put('/hospedes/', data=data)
-    assert response.status_code == 400
