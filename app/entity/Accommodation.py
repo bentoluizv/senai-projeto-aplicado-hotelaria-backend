224b702dd59689979d050c2c531d4f20e6e160d9
bentoluizv <bentoluizv@gmail.com>
2024-05-29 01:33:45 -0300

    implemented validation logic in the Accommodation entity using pydantic

--- app/entity/Accommodation.py
+++ app/entity/Accommodation.py
@@ -1,79 +1,28 @@
 from datetime import datetime
-from typing import TypedDict
+from uuid import UUID, uuid4
 
-#TODO: Implementar a validação dos dados de entrada da classe.
+from pydantic import BaseModel, Field
 
-class AccommodationDTO(TypedDict):
-    uuid: str
+
+
+class Accommodation(BaseModel):
+    uuid: str = Field(default=str(uuid4()))
     name: str
     status: str
-    total_guests: str
-    single_beds: str
-    double_beds: str
-    min_nights: str
-    price: str  
-    created_at: str | None
-     
+    total_guests: int
+    single_beds: int
+    double_beds: int
+    min_nights: int = Field(default=2)
+    price: int
+    created_at: str = Field(default=datetime.now())
 
-class UpdatableAccommodation(TypedDict):   
-    name: str
-    status: str
-    total_guests: str
-    single_beds: str
-    double_beds: str
-    min_nights: str
-    price: str  
-    created_at: str | None
-
-class Accommodation:
-    def __init__(
-            self,
-            uuid: str,
-            name: str,
-            status: str,
-            total_guests: str,
-            single_beds: str,
-            double_beds: str,
-            min_nights: str,
-            price: str,
-            created_at: str | None = None):
-        self.uuid = uuid
-        self.name = name
-        self.status = status
-        self.total_guests = total_guests
-        self.single_beds = single_beds
-        self.single_beds = single_beds
-        self.double_beds = double_beds
-        self.min_nights = min_nights
-        self.price = price
-        self.created_at = datetime.fromisoformat(created_at) if created_at else datetime.now()
 
     @classmethod
-    def from_dict(cls, accommodation_dto: AccommodationDTO):
-        return cls(
-            accommodation_dto['uuid'],
-            accommodation_dto['name'],
-            accommodation_dto['status'],
-            accommodation_dto['total_guests'],
-            accommodation_dto['single_beds'],
-            accommodation_dto['double_beds'],
-            accommodation_dto['min_nights'],          
-            accommodation_dto['price'],
-            accommodation_dto['created_at']
-            
-        )
+    def from_dict(cls, data):
+        return cls(**data)
 
-    def to_dict(self) -> AccommodationDTO:
-        return {
-            'uuid': self.uuid,
-            'name': self.name,
-            'status': self.status,
-            'total_guests': self.total_guests,
-            'single_beds': self.single_beds,
-            'double_beds': self.double_beds,
-            'min_nights': self.min_nights,
-            'price': self.price,
-            'created_at': self.created_at.isoformat()
-        }
-    
-        
+    def to_dict(self):
+        return self.model_dump()
+
+    def to_json(self):
+        return self.model_dump_json()
