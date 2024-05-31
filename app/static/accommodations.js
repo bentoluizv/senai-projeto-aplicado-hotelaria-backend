a981e2892bc81799a748bb209a0381dae043c273
bentoluizv <bentoluizv@gmail.com>
2024-05-30 22:45:42 -0300

    feature: update accommodation page and partial logic

--- app/static/accommodations.js
+++ app/static/accommodations.js
@@ -8,5 +8,5 @@ const tableBody = document.getElementById("table_content");
 
 tableBody.addEventListener("click", (event) => {
   const uuid = event.target.parentNode.cells.namedItem("uuid").textContent;
-  console.log(uuid);
+  window.location.href = `http://127.0.0.1:5000/acomodacoes/${uuid}`;
 });
