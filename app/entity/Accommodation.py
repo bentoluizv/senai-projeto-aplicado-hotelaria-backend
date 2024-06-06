19eaaf34c590930dad76a215905d9bbe1ad6650d
bentoluizv <bentoluizv@gmail.com>
2024-06-06 18:48:45 -0300

    implementing validation for Accommodation and respective test suite

--- app/entity/Accommodation.py
+++ app/entity/Accommodation.py
@@ -1,23 +1,28 @@
 from datetime import datetime
 from typing import List
-from uuid import uuid4
+from uuid import UUID, uuid4
 
-from pydantic import BaseModel, Field
+from pydantic import (
+    Field,
+    field_serializer,
+    field_validator,
+    model_validator,
+)
+
+from app.utils.strict_model import StrictModel
 
 
-
-class Accommodation(BaseModel):
-    uuid: str = Field(default=str(uuid4()))
+class Accommodation(StrictModel):
+    uuid: UUID = Field(default=uuid4())
     name: str
-    status: str = Field(default="Dísponivel")
+    status: str = Field(default="Disponível")
     total_guests: int
     single_beds: int
     double_beds: int
     min_nights: int = Field(default=2)
     price: int
-    created_at: str = Field(default=datetime.now().isoformat())
-    amenities: List[str] = Field(default=[])
-
+    created_at: datetime = Field(default=datetime.now())
+    amenities: List[str]
 
     @classmethod
     def from_dict(cls, data):
@@ -28,3 +33,72 @@ class Accommodation(BaseModel):
 
     def to_json(self):
         return self.model_dump_json()
+
+    @model_validator(mode="before")
+    def validate_input_data_params(cls, data):
+        parsed_data = {
+            "uuid": UUID(data["uuid"]),
+            "name": data["name"],
+            "status": data["status"],
+            "total_guests": int(data["total_guests"]),
+            "single_beds": int(data["single_beds"]),
+            "double_beds": int(data["double_beds"]),
+            "min_nights": int(data["min_nights"]),
+            "price": int(data["price"]),
+            "created_at": datetime.fromisoformat(data["created_at"]),
+            "amenities": data["amenities"],
+        }
+        return parsed_data
+
+    @model_validator(mode="after")
+    def validade_name_is_not_an_empty_strings(self):
+        if not self.name:
+            raise ValueError("Name is probably empty")
+
+        return self
+
+    @field_validator("status")
+    @classmethod
+    def validate_status(cls, value):
+        expected_statuses = ["Disponível", "Reservado", "Aguardando Limpeza"]
+        if value not in expected_statuses:
+            raise ValueError(
+                f"Invalid status! [{value}] does not match any expected status: {expected_statuses}"
+            )
+        return value
+
+    @field_validator("min_nights")
+    @classmethod
+    def validate_min_nights(cls, value):
+        if value <= 0:
+            raise ValueError("Minimum number of nights must be greater than one")
+        return value
+
+    @field_validator("price")
+    @classmethod
+    def validate_price(cls, value):
+        if value <= 0:
+            raise ValueError("Invalid Price")
+        return value
+
+    @model_validator(mode="after")
+    def validate_amount_of_beds(self):
+        if self.double_beds <= 0 and self.single_beds <= 0:
+            raise ValueError(
+                "Accommodation must have at least one type of bed available"
+            )
+        return self
+
+    @field_serializer("created_at")
+    def serialize_created_at(self, created_at: datetime):
+        return created_at.isoformat()
+
+    @field_serializer("uuid")
+    def serialize_uuid(self, uuid):
+        return str(uuid)
+
+    @field_serializer(
+        "total_guests", "single_beds", "double_beds", "min_nights", "price"
+    )
+    def serialize_interger_properties_to_str(self, value):
+        return str(value)
---
+++ tests/unit/test_accommodation.py
@@ -0,0 +1,88 @@
+import json
+
+import pytest
+from click import echo
+
+from app.entity.Accommodation import Accommodation
+
+
+@pytest.fixture()
+def accommodation_data():
+    data = {
+        "uuid": "ff90f824-5938-4c1a-8ad1-d558dc776470",
+        "name": "Quarto Individual",
+        "status": "Disponível",
+        "total_guests": "1",
+        "single_beds": "1",
+        "double_beds": "0",
+        "min_nights": "2",
+        "price": "180",
+        "created_at": "2024-05-22T10:56:45.439704",
+        "amenities": [],
+    }
+    yield data
+
+
+def test_should_instanciate_a_valid_accommodation(accommodation_data):
+    accommodation = Accommodation.from_dict(accommodation_data)
+    assert isinstance(accommodation, Accommodation)
+
+
+def test_should_serialize_correctly_to_an_equivalent_dict(accommodation_data):
+    guest = Accommodation.from_dict(accommodation_data)
+    guest_dict = guest.to_dict()
+    assert guest_dict == accommodation_data
+
+
+def test_should_serialize_correctly_to_a_json_equivalent(accommodation_data):
+    accommodation = Accommodation.from_dict(accommodation_data)
+    accommodation_json = accommodation.to_json()
+    echo(json.dumps(accommodation_data, separators=(",", ":")))
+    assert accommodation_json == json.dumps(
+        accommodation_data, separators=(",", ":"), ensure_ascii=False
+    )
+
+
+def test_should_raise_an_error_when_passing_an_empty_string_to_name(accommodation_data):
+    accommodation_data["name"] = ""
+    with pytest.raises(ValueError):
+        Accommodation.from_dict(accommodation_data)
+
+
+def test_should_raise_an_error_when_passing_either_single_and_double_beds(
+    accommodation_data,
+):
+    accommodation_data["single_beds"] = "0"
+    accommodation_data["double_beds"] = "0"
+    with pytest.raises(ValueError):
+        Accommodation.from_dict(accommodation_data)
+
+
+def test_should_raise_an_error_when_passing_created_at_with_a_string_not_in_iso_format(
+    accommodation_data,
+):
+    accommodation_data["created_at"] = ""
+    with pytest.raises(ValueError):
+        Accommodation.from_dict(accommodation_data)
+
+
+def test_should_raise_an_error_when_price_is_less_or_equal_to_zero(
+    accommodation_data,
+):
+    accommodation_data["price"] = "0"
+    with pytest.raises(ValueError):
+        Accommodation.from_dict(accommodation_data)
+
+
+def test_should_raise_an_error_when_min_nights_is_less_or_equal_to_zero(
+    accommodation_data,
+):
+    accommodation_data["min_nights"] = "0"
+    with pytest.raises(ValueError):
+        Accommodation.from_dict(accommodation_data)
+
+
+def test_should_raise_an_error_when_reassigning_any_property(accommodation_data):
+    accommodation = Accommodation.from_dict(accommodation_data)
+    with pytest.raises(ValueError):
+        accommodation.name = "Novo Quarto"
