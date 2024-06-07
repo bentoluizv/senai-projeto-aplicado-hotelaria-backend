78cf9c01f788a1d53c75d8d2790415f1a0b3fa72
bentoluizv <bentoluizv@gmail.com>
2024-06-07 13:45:16 -0300

    transform now handles accommodation keys

--- app/utils/transform.py
+++ app/utils/transform.py
@@ -9,6 +9,15 @@ def transform(obj: dict):
         if k == "created_at" or k == "check_in" or k == "check_out":
             new_obj[k] = datetime.fromisoformat(v)
 
+        if (
+            k == "total_guests"
+            or k == "single_beds"
+            or k == "double_beds"
+            or k == "min_nights"
+            or k == "price"
+        ):
+            new_obj[k] = int(v)
+
         if k == "uuid":
             new_obj[k] = UUID(v)
 
