0e82af2b8d852392a53b7cea3bbd2fcef6b7ac36
bentoluizv <bentoluizv@gmail.com>
2024-06-07 13:31:11 -0300

    added standardization of spacing and double quotes,  remove unused imports

--- app/accommodations.py
+++ app/accommodations.py
@@ -1,33 +1,36 @@
-from flask import render_template
-from flask import Blueprint
+from flask import Blueprint, render_template
+from markupsafe import escape
 
 from app.data.dao.AccommodationDAO import AccommodationDAO
 from app.data.database.db import get_db
 from app.data.repositories.AccommodationRepository import AccommodationtRepository
-from markupsafe import escape
 
-bp = Blueprint('accommodation', __name__, url_prefix='/acomodacoes')
+bp = Blueprint("accommodation", __name__, url_prefix="/acomodacoes")
 
-@bp.get('/')
+
+@bp.get("/")
 def get_accommodations():
     db = get_db()
     dao = AccommodationDAO(db)
     repository = AccommodationtRepository(dao)
-    accommodation = [ accommodation.to_dict() for accommodation in repository.find_many() ]
-    return render_template('accommodations.html', rows=accommodation)
+    accommodation = [
+        accommodation.to_dict() for accommodation in repository.find_many()
+    ]
+    return render_template("accommodations.html", rows=accommodation)
 
 
-@bp.get('/cadastro/')
+@bp.get("/cadastro/")
 def new():
-    return render_template('newAccommodation.html')
+    return render_template("newAccommodation.html")
 
 
-@bp.get('/<uuid>/')
+@bp.get("/<uuid>/")
 def get_accommodation(uuid):
     url_param = escape(uuid)
     db = get_db()
     dao = AccommodationDAO(db)
     repository = AccommodationtRepository(dao)
-    accommodation = repository.findBy('uuid', str(url_param))
-    return render_template('updateAccommodation.html', accommodation=accommodation.to_dict())
-
+    accommodation = repository.findBy("uuid", str(url_param))
+    return render_template(
+        "updateAccommodation.html", accommodation=accommodation.to_dict()
+    )
