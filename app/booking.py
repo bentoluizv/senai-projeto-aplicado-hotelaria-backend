fd40caae73a3a81d16b60c5d4a4f47ba158763c5
bentoluizv <bentoluizv@gmail.com>
2024-06-07 13:31:00 -0300

    added standardization of spacing and double quotes,  remove unused imports

--- app/booking.py
+++ app/booking.py
@@ -1,19 +1,16 @@
-from flask import render_template
-from flask import Blueprint
-from markupsafe import escape
+from flask import Blueprint, render_template
 
 from app.data.dao.BookingDAO import BookingDAO
 from app.data.database.db import get_db
 from app.data.repositories.BookingRepository import BookingRepository
-from app.entity.Booking import Booking
+
+bp = Blueprint("booking", __name__, url_prefix="/")
 
 
-bp = Blueprint('booking', __name__, url_prefix='/')
-
-@bp.get('/')
+@bp.get("/")
 def booking():
     db = get_db()
     dao = BookingDAO(db)
     respository = BookingRepository(dao)
-    bookings = [ guest.to_dict() for guest in respository.find_many() ]
-    return render_template('index.html', rows=bookings)
+    bookings = [guest.to_dict() for guest in respository.find_many()]
+    return render_template("index.html", rows=bookings)
