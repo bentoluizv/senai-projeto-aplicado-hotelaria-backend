b54e018b4d59df7c6bd8a498a936b7078d803df2
bentoluizv <bentoluizv@gmail.com>
2024-06-07 11:26:41 -0300

    implementing validation for Booking and respective test suite

--- app/entity/Booking.py
+++ app/entity/Booking.py
@@ -1,17 +1,23 @@
 from datetime import datetime
-from uuid import uuid4
-from pydantic import BaseModel, Field
+from uuid import UUID, uuid4
+
+from pydantic import (
+    Field,
+    field_validator,
+    model_validator,
+)
 
 from app.entity.Accommodation import Accommodation
 from app.entity.Guests import Guest
+from app.utils.StrictModel import StrictModel
 
 
-class Booking(BaseModel):
-    uuid: str = Field(default=str(uuid4()))
-    created_at: str = Field(default=datetime.now().isoformat())
-    status: str = Field(default='Aguardando Check-in')
-    check_in: str
-    check_out: str
+class Booking(StrictModel):
+    uuid: UUID = Field(default=(uuid4()))
+    created_at: datetime = Field(default=datetime.now())
+    status: str = Field(default="Aguardando Check-in")
+    check_in: datetime
+    check_out: datetime
     guest: Guest
     accommodation: Accommodation
 
@@ -24,3 +30,24 @@ class Booking(BaseModel):
 
     def to_json(self):
         return self.model_dump_json()
+
+    @field_validator("status")
+    @classmethod
+    def validate_status(cls, value):
+        expected_statuses = [
+            "Aguardando Check-In",
+            "Reserva Ativa",
+            "Aguardando Check-Out",
+            "Finalizada",
+        ]
+        if value not in expected_statuses:
+            raise ValueError(
+                f"Invalid status! [{value}] does not match any expected status: {expected_statuses}"
+            )
+        return value
+
+    @model_validator(mode="after")
+    def validate_check_in_check_out(self):
+        if self.check_out < self.check_in:
+            raise ValueError("Check out date is before check in")
+        return self
