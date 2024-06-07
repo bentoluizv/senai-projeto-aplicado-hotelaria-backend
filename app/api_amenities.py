76e2d11c1b976b96b608813d4dc6a3d40dd3207d
bentoluizv <bentoluizv@gmail.com>
2024-06-07 13:30:30 -0300

    added standardization of spacing and double quotes

--- app/api_amenities.py
+++ app/api_amenities.py
@@ -6,16 +6,16 @@ from pydantic import ValidationError
 from app.data.dao.AmenitieDAO import AmenitieDAO
 from app.data.database.db import get_db
 
+bp = Blueprint("api_amenities", __name__, url_prefix="/api/amenities")
 
-bp = Blueprint('api_amenities', __name__, url_prefix='/api/amenities')
 
-@bp.get('')
+@bp.get("")
 def get_amenities():
     db = get_db()
     dao = AmenitieDAO(db)
 
     try:
-        amenities = [ amenitie for amenitie in dao.find_many() ]
+        amenities = [amenitie for amenitie in dao.find_many()]
         return jsonify(amenities)
 
     except ValidationError as err:
@@ -23,26 +23,26 @@ def get_amenities():
         abort(500)
 
 
-@bp.post('/cadastro')
+@bp.post("/cadastro")
 def create_amenitie():
     if request.form is None:
         abort(400)
 
-    amenitie = request.form['amenitie'],
+    amenitie = (request.form["amenitie"],)
 
     db = get_db()
     dao = AmenitieDAO(db)
 
     try:
         dao.insert(amenitie)
-        return 'CREATED', 201
+        return "CREATED", 201
 
     except ValueError as e:
         click.echo(e)
         abort(400)
 
 
-@bp.delete('/<name>')
+@bp.delete("/<name>")
 def delete_accommodation(name):
     db = get_db()
     dao = AmenitieDAO(db)
@@ -50,7 +50,7 @@ def delete_accommodation(name):
 
     try:
         dao.delete(str(url_param))
-        return 'DELETED', 200
+        return "DELETED", 200
 
     except ValueError:
         abort(404)
