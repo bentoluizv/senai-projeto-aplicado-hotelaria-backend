a585784531d7e3034e24efd880dbb9c7aae5909e
bentoluizv <bentoluizv@gmail.com>
2024-06-07 12:14:35 -0300

    fixed accommodation repository to adapt to changes in the entity

--- app/data/repositories/AccommodationRepository.py
+++ app/data/repositories/AccommodationRepository.py
@@ -1,45 +1,50 @@
+from datetime import datetime
 from typing import List
+from uuid import UUID
 
-import click
 from app.data.dao.AccommodationDAO import AccommodationDAO
 from app.entity.Accommodation import Accommodation
 
 
 class AccommodationtRepository:
     def __init__(self, dao: AccommodationDAO):
-        self.dao  = dao
-
+        self.dao = dao
 
     def count(self) -> int:
         return self.dao.count()
 
-
     def insert(self, accommodation: Accommodation):
-        exists  = self.dao.find('uuid', accommodation.uuid)
+        exists = self.dao.find("uuid", str(accommodation.uuid))
 
         if exists:
-            raise ValueError(f'Accommodation with document {accommodation.uuid} already exists')
+            raise ValueError(
+                f"Accommodation with document {accommodation.uuid} already exists"
+            )
 
-        self.dao.insert(accommodation.to_dict())
+        accommodation_dict = accommodation.to_dict()
+        accommodation_dict["uuid"] = str(accommodation_dict["uuid"])
+        accommodation_dict["created_at"] = accommodation_dict["created_at"].isoformat()
 
+        self.dao.insert(accommodation_dict)
 
     def findBy(self, property: str, value: str):
         exists = self.dao.find(property, value)
 
         if not exists:
-            raise ValueError(f'Accommodation with {property} {value} not exists')
+            raise ValueError(f"Accommodation with {property} {value} not exists")
 
-        if (exists['amenities'] is not None):
-            amenities = exists['amenities']
-            exists['amenities'] = amenities.split(',')
+        if exists["amenities"] is not None:
+            amenities = exists["amenities"]
+            exists["amenities"] = amenities.split(",")
         else:
-            exists['amenities'] = []
+            exists["amenities"] = []
 
+        exists["uuid"] = UUID(exists["uuid"])
+        exists["created_at"] = datetime.fromisoformat(exists["created_at"])
         accommodation = Accommodation.from_dict(exists)
         return accommodation
 
-
-    def find_many(self) :
+    def find_many(self):
         rawAccommodations = self.dao.find_many()
         if len(rawAccommodations) == 0:
             return []
@@ -47,42 +52,46 @@ class AccommodationtRepository:
         accommodations: List[Accommodation] = []
 
         for raw in rawAccommodations:
-            if(raw['amenities'] is not None):
-                amenities = raw['amenities']
-                raw['amenities'] = amenities.split(',')
+            if raw["amenities"] is not None:
+                amenities = raw["amenities"]
+                raw["amenities"] = amenities.split(",")
             else:
-                raw['amenities'] = []
+                raw["amenities"] = []
+
+            raw["uuid"] = UUID(raw["uuid"])
+            raw["created_at"] = datetime.fromisoformat(raw["created_at"])
 
             accommodation = Accommodation.from_dict(raw)
             accommodations.append(accommodation)
 
         return accommodations
 
-
     def update(self, accommodation: Accommodation):
-        exists = self.dao.find('uuid', accommodation.uuid)
+        exists = self.dao.find("uuid", str(accommodation.uuid))
 
         if not exists:
-            raise ValueError(f'Accommodation with document {accommodation.uuid} not exists')
+            raise ValueError(
+                f"Accommodation with document {accommodation.uuid} not exists"
+            )
 
         self.dao.update(
-            accommodation.uuid,
+            str(accommodation.uuid),
             {
-                'name': accommodation.name,
-                'status': accommodation.status,
-                'total_guests': accommodation.total_guests,
-                'single_beds': accommodation.single_beds,
-                'double_beds': accommodation.double_beds,
-                'min_nights': accommodation.min_nights,
-                'price': accommodation.price,
-                'amenities': accommodation.amenities
-            })
-
+                "name": accommodation.name,
+                "status": accommodation.status,
+                "total_guests": accommodation.total_guests,
+                "single_beds": accommodation.single_beds,
+                "double_beds": accommodation.double_beds,
+                "min_nights": accommodation.min_nights,
+                "price": accommodation.price,
+                "amenities": accommodation.amenities,
+            },
+        )
 
     def delete(self, uuid: str):
-        exists = self.dao.find('uuid', uuid)
+        exists = self.dao.find("uuid", uuid)
 
         if not exists:
-            raise ValueError(f'Accommodation with document {uuid} not exists')
+            raise ValueError(f"Accommodation with document {uuid} not exists")
 
         self.dao.delete(uuid)
--- tests/integration/test_repository_accommodation.py
+++ tests/integration/test_repository_accommodation.py
@@ -1,11 +1,15 @@
-from pytest import fixture
+from datetime import datetime
+from uuid import UUID
+
 import pytest
+
 from app.data.dao.AccommodationDAO import AccommodationDAO
 from app.data.database.db import get_db
 from app.data.repositories.AccommodationRepository import AccommodationtRepository
 from app.entity.Accommodation import Accommodation
 
-@fixture
+
+@pytest.fixture
 def repository(app):
     with app.app_context():
         db = get_db()
@@ -14,23 +18,23 @@ def repository(app):
         yield repository
 
 
-
 def test_should_count_the_numbers_rows(repository):
     assert repository.count() == 6
 
 
 def test_should_create_a_accommodation(repository):
     accommodation_dto = {
-        'uuid': 'ff90f824-5938-4c1a-8ad1-d558dc776470',
-        'name':'Quarto Individual',
-        'status':'Disponivel',
-        'total_guests': '1',
-        'single_beds': '1',
-        'double_beds': '0',
-        'min_nights': '2',
-        'price':'180',
-        'created_at': '2024-05-22T10:56:45.439704'
-        }
+        "uuid": UUID("ff90f824-5938-4c1a-8ad1-d558dc776470"),
+        "name": "Quarto Individual",
+        "status": "Disponível",
+        "total_guests": 1,
+        "single_beds": 1,
+        "double_beds": 0,
+        "min_nights": 2,
+        "price": 180,
+        "created_at": datetime.fromisoformat("2024-05-22T10:56:45.439704"),
+        "amenities": [],
+    }
 
     accommodation = Accommodation.from_dict(accommodation_dto)
     repository.insert(accommodation)
@@ -40,7 +44,7 @@ def test_should_create_a_accommodation(repository):
 
 def test_should_return_one_accommodation_by_its_name(repository):
     accommodation = repository.findBy("name", "Domo")
-    assert accommodation.uuid == 'bcadaaf8-a036-42d5-870c-de7b24792abf'
+    assert accommodation.uuid == UUID("bcadaaf8-a036-42d5-870c-de7b24792abf")
 
 
 def test_should_return_all_accommodations(repository):
@@ -51,20 +55,20 @@ def test_should_return_all_accommodations(repository):
 
 def test_repository_update(repository):
     accommodation_dto = {
-        'uuid': "bcadaaf8-a036-42d5-870c-de7b24792abf",
-        'name':'Quarto Individual',
-        'status':'Disponivel',
-        'total_guests': '1',
-        'single_beds': '1',
-        'double_beds': '0',
-        'min_nights': '2',
-        'price':'180',
-        'amenities': ['ducha']
-        }
-    accommodation_to_update =  Accommodation.from_dict(accommodation_dto)
+        "uuid": UUID("bcadaaf8-a036-42d5-870c-de7b24792abf"),
+        "name": "Quarto Individual",
+        "status": "Disponível",
+        "total_guests": 1,
+        "single_beds": 1,
+        "double_beds": 0,
+        "min_nights": 2,
+        "price": 180,
+        "amenities": ["ducha"],
+    }
+    accommodation_to_update = Accommodation.from_dict(accommodation_dto)
     repository.update(accommodation_to_update)
-    accommodation = repository.findBy("uuid","bcadaaf8-a036-42d5-870c-de7b24792abf")
-    assert accommodation.name == 'Quarto Individual'
+    accommodation = repository.findBy("uuid", "bcadaaf8-a036-42d5-870c-de7b24792abf")
+    assert accommodation.name == "Quarto Individual"
 
 
 def test_should_delete_one_accommodation_by_its_uuid(repository):
