ea67bfadc1d422304c8cf988904f5d44db85f5f3
bentoluizv <bentoluizv@gmail.com>
2024-06-07 13:31:30 -0300

    added standardization of spacing and double quotes,  remove unused imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 """Flask Application Factory"""
 
-from http.client import HTTPException
 import os
-from flask import Flask, json
-from flask import render_template
+
+from flask import Flask, json, render_template
+
 from .data.database import db
 
 
@@ -16,7 +16,7 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
 
@@ -28,61 +28,66 @@ def create_app(test_config=None):
     @app.errorhandler(404)
     def resource_not_found(e):
         response = e.get_response()
-        response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-    })
+        response.data = json.dumps(
+            {
+                "code": e.code,
+                "name": e.name,
+                "description": e.description,
+            }
+        )
         response.content_type = "application/json"
         return response
 
     @app.errorhandler(409)
     def resource_already_exists(e):
         response = e.get_response()
-        response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-    })
+        response.data = json.dumps(
+            {
+                "code": e.code,
+                "name": e.name,
+                "description": e.description,
+            }
+        )
         response.content_type = "application/json"
         return response
 
     @app.errorhandler(400)
     def bad_request(e):
         response = e.get_response()
-        response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-    })
+        response.data = json.dumps(
+            {
+                "code": e.code,
+                "name": e.name,
+                "description": e.description,
+            }
+        )
         response.content_type = "application/json"
         return response
 
-    @app.get('/')
+    @app.get("/")
     def index():
-        return render_template('index.html')
+        return render_template("index.html")
 
-
-    @app.get('/hello')
+    @app.get("/hello")
     def hello():
-        return 'Hello, World!'
+        return "Hello, World!"
 
     db.init_app(app)
 
-    from . import  guests
-    from . import  api_guest
+    from . import api_guest, guests
+
     app.register_blueprint(guests.bp)
     app.register_blueprint(api_guest.bp)
-    from . import  accommodations
-    from . import api_accommodations
+    from . import accommodations, api_accommodations
+
     app.register_blueprint(accommodations.bp)
     app.register_blueprint(api_accommodations.bp)
     from . import api_amenities
+
     app.register_blueprint(api_amenities.bp)
-    from . import api_booking
-    from . import booking
+    from . import api_booking, booking
+
     app.register_blueprint(booking.bp)
     app.register_blueprint(api_booking.bp)
-
 
     return app
