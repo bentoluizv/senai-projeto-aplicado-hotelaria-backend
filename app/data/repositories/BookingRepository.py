3fd1c7579cc3db526a9f8a406f58aac6dfc46b90
bentoluizv <bentoluizv@gmail.com>
2024-06-07 13:15:48 -0300

    fixed booking repository to adapt to changes in the entity

--- app/data/repositories/BookingRepository.py
+++ app/data/repositories/BookingRepository.py
@@ -2,48 +2,51 @@ from typing import List
 
 from app.data.dao.BookingDAO import BookingDAO, BookingDTO
 from app.entity.Booking import Booking
+from app.utils.transform import transform
 
 
 class BookingRepository:
     def __init__(self, dao: BookingDAO):
-        self.dao  = dao
-
+        self.dao = dao
 
     def count(self) -> int:
         return self.dao.count()
 
-
     def insert(self, booking: Booking):
-        exists  = self.dao.findBy('uuid',booking.uuid)
+        exists = self.dao.findBy("uuid", str(booking.uuid))
 
         if exists:
-            raise ValueError(f'Reserva com id {booking.uuid} já está cadastrado')
+            raise ValueError(f"Reserva com id {booking.uuid} já está cadastrado")
 
         booking_dict = booking.to_dict()
 
         booking_dto: BookingDTO = {
-            'status': booking_dict['status'],
-            'check_in': booking_dict['check_in'],
-            'created_at': booking_dict['created_at'],
-            'uuid': booking_dict['uuid'],
-            'check_out': booking_dict['check_out'],
-            'guest_document': booking_dict['guest']['document'],
-            'accommodation_uuid': booking_dict['accommodation']['uuid'],
+            "status": booking_dict["status"],
+            "check_in": booking_dict["check_in"].isoformat(),
+            "created_at": booking_dict["created_at"].isoformat(),
+            "uuid": str(booking_dict["uuid"]),
+            "check_out": booking_dict["check_out"].isoformat(),
+            "guest_document": booking_dict["guest"]["document"],
+            "accommodation_uuid": str(booking_dict["accommodation"]["uuid"]),
         }
 
         self.dao.insert(booking_dto)
 
-
     def find(self, uuid: str):
-        exists = self.dao.findBy('uuid',uuid)
+        exists = self.dao.findBy("uuid", uuid)
 
         if not exists:
-            raise ValueError(f'Reserva com  id {uuid} não está cadastrado')
+            raise ValueError(f"Reserva com  id {uuid} não está cadastrado")
 
-        booking = Booking.from_dict(exists)
+        if exists["accommodation"]["amenities"] is not None:
+            amenities = exists["accommodation"]["amenities"]
+            exists["accommodation"]["amenities"] = amenities.split(",")
+        else:
+            exists["accommodation"]["amenities"] = []
+
+        booking = Booking.from_dict(transform(exists))
 
         return booking
-
 
     def find_many(self) -> List[Booking]:
         existing = self.dao.find_many()
@@ -51,36 +54,40 @@ class BookingRepository:
         if len(existing) == 0:
             return []
 
-        guests: List[Booking] = []
+        bookings: List[Booking] = []
 
         for unknown in existing:
-            guest = Booking.from_dict(unknown)
-            guests.append(guest)
+            if unknown["accommodation"]["amenities"] is not None:
+                amenities = unknown["accommodation"]["amenities"]
+                unknown["accommodation"]["amenities"] = amenities.split(",")
+            else:
+                unknown["accommodation"]["amenities"] = []
 
-        return guests
-
+            booking = Booking.from_dict(transform(unknown))
+            bookings.append(booking)
+        return bookings
 
     def update(self, booking: Booking):
-        exists = self.dao.findBy('uuid',booking.uuid)
+        exists = self.dao.findBy("uuid", str(booking.uuid))
 
         if not exists:
-            raise ValueError(f'A Reserva com o id {booking.uuid} não está cadastrada')
+            raise ValueError(f"A Reserva com o id {booking.uuid} não está cadastrada")
 
         self.dao.update(
-            booking.uuid,
+            str(booking.uuid),
             {
-                'status': booking.status,
-                'check_in': booking.check_in,
-                'check_out': booking.check_out,
-                'guest_document':   booking.guest.document,
-                'accommodation_uuid': booking.accommodation.uuid
-            })
-
+                "status": booking.status,
+                "check_in": booking.check_in.isoformat(),
+                "check_out": booking.check_out.isoformat(),
+                "guest_document": booking.guest.document,
+                "accommodation_uuid": str(booking.accommodation.uuid),
+            },
+        )
 
     def delete(self, uuid: str):
-        exists = self.dao.findBy('uuid',uuid)
+        exists = self.dao.findBy("uuid", uuid)
 
         if not exists:
-            raise ValueError(f'Reserva com o id {uuid} não está cadastrada')
+            raise ValueError(f"Reserva com o id {uuid} não está cadastrada")
 
         self.dao.delete(uuid)
--- tests/integration/test_repository_booking.py
+++ tests/integration/test_repository_booking.py
@@ -1,9 +1,14 @@
-from pytest import fixture
+from datetime import datetime
+from uuid import UUID
+
 import pytest
+from pytest import fixture
+
 from app.data.dao.BookingDAO import BookingDAO
 from app.data.database.db import get_db
 from app.data.repositories.BookingRepository import BookingRepository
 from app.entity.Booking import Booking
+
 
 @fixture
 def repository(app):
@@ -14,38 +19,45 @@ def repository(app):
         yield repository
 
 
-
 def test_should_count_how_many_records_are_registered(repository):
     assert repository.count() == 4
 
 
 def test_should_create_a_new_booking(repository):
     booking_dto = {
-        'uuid': 'dd093495-b637-4ff8-bf2c-eb99d0f88031',
-        'created_at': '2024-06-03T18:38:35.447990',
-        'status': 'Aguardando Checkin',
-        'check_in': '2024-09-15T08:30:00',
-        'check_out': '2024-09-18T12:30:00',
-        'guest': {
-            'document': '00157624242',
-            'name': 'Bento Luiz',
-            'surname': 'Vervloet Machado da Silva Neto',
-            'country': 'Brazil',
-            'created_at': '2024-03-15T10:30:00',
-            'phone':  '48992054211'
+        "uuid": UUID("dd093495-b637-4ff8-bf2c-eb99d0f88031"),
+        "created_at": datetime.fromisoformat("2024-06-03T18:38:35.447990"),
+        "status": "Aguardando Check-In",
+        "check_in": datetime.fromisoformat("2024-09-15T08:30:00"),
+        "check_out": datetime.fromisoformat("2024-09-18T12:30:00"),
+        "guest": {
+            "document": "00157624242",
+            "name": "Bento Luiz",
+            "surname": "Vervloet Machado da Silva Neto",
+            "country": "Brazil",
+            "created_at": datetime.fromisoformat("2024-03-15T10:30:00"),
+            "phone": "48992054211",
         },
-        'accommodation': {
-            'uuid': 'bcadaaf8-a036-42d5-870c-de7b24792abf',
-            'name': 'Domo',
-            'status': 'Disponível',
-            'total_guests': 2,
-            'single_beds': 0,
-            'double_beds': 1,
-            'min_nights': 2,
-            'price': 590,
-            'created_at': '2000-01-01T00:00:00',
-            'amenities': ['ar-condicionado', 'wifi', 'tv', 'frigobar', 'ducha', 'cozinha', 'toalhas']
-        }
+        "accommodation": {
+            "uuid": UUID("bcadaaf8-a036-42d5-870c-de7b24792abf"),
+            "name": "Domo",
+            "status": "Disponível",
+            "total_guests": 2,
+            "single_beds": 0,
+            "double_beds": 1,
+            "min_nights": 2,
+            "price": 590,
+            "created_at": datetime.fromisoformat("2000-01-01T00:00:00"),
+            "amenities": [
+                "ar-condicionado",
+                "wifi",
+                "tv",
+                "frigobar",
+                "ducha",
+                "cozinha",
+                "toalhas",
+            ],
+        },
     }
     booking = Booking.from_dict(booking_dto)
     repository.insert(booking)
@@ -54,48 +66,47 @@ def test_should_create_a_new_booking(repository):
 
 def test_should_find_a_booking_by_its_uuid(repository):
     booking = repository.find("e08f76e8-0e71-4a48-a85a-bf7e8f61479e")
-    assert booking.guest.name ==  'Bento Luiz'
-
+    assert booking.guest.name == "Bento Luiz"
 
 
 def test_should_return_all_bookings_registered(repository):
     bookings = repository.find_many()
     assert len(bookings) == 4
-    assert  bookings[0].guest.name == 'Bento Luiz'
+    assert bookings[0].guest.name == "Bento Luiz"
 
 
 def test_shoud_update_an_existing_booking(repository):
     updated_booking_data = {
-        'uuid':"e08f76e8-0e71-4a48-a85a-bf7e8f61479e",
-        'status': 'Finalizada',
-        'check_in': "2024-06-15T08:30:00",
-        'check_out': "2024-06-18T17:30:00",
-        'guest': {
-            'document': '00157624242',
-            'name': 'Bento Luiz',
-            'surname': 'Vervloet Machado da Silva Neto',
-            'country': 'Brazil',
-            'created_at': '2024-03-15T10:30:00',
-            'phone':  '48992054211'
+        "uuid": UUID("e08f76e8-0e71-4a48-a85a-bf7e8f61479e"),
+        "status": "Finalizada",
+        "check_in": datetime.fromisoformat("2024-06-15T08:30:00"),
+        "check_out": datetime.fromisoformat("2024-06-18T17:30:00"),
+        "guest": {
+            "document": "00157624242",
+            "name": "Bento Luiz",
+            "surname": "Vervloet Machado da Silva Neto",
+            "country": "Brazil",
+            "created_at": datetime.fromisoformat("2024-03-15T10:30:00"),
+            "phone": "48992054211",
         },
-        'accommodation': {
-            'uuid': "242d5665-aa90-429a-95d5-767515ff8ccc",
-            'name': '"Estacionamento para overlanders"',
-            'status': "Disponível",
-            'total_guests': 4,
-            'single_beds': 0,
-            'double_beds': 0,
-            'min_nights': 2,
-            'price': 100,
-            'created_at': "2000-01-01T00:15:00",
-            'amenities': ['ducha']
-        }
-            }
+        "accommodation": {
+            "uuid": UUID("242d5665-aa90-429a-95d5-767515ff8ccc"),
+            "name": "Estacionamento para overlanders",
+            "status": "Disponível",
+            "total_guests": 4,
+            "single_beds": 0,
+            "double_beds": 0,
+            "min_nights": 2,
+            "price": 100,
+            "created_at": datetime.fromisoformat("2000-01-01T00:15:00"),
+            "amenities": ["ducha"],
+        },
+    }
 
     booking = Booking.from_dict(updated_booking_data)
     repository.update(booking)
     updated_booking = repository.find("e08f76e8-0e71-4a48-a85a-bf7e8f61479e")
-    assert updated_booking.status == 'Finalizada'
+    assert updated_booking.status == "Finalizada"
 
 
 def test_repository_delete(repository):
