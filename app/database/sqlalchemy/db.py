5e34b1bab7478c4b096a60e8ece77a0fc2772727
Bento Luiz <bentoluizv@gmail.com>
2024-06-26 11:41:14 -0300

    remove flask and sqlite3 database

--- app/database/sqlalchemy/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-
-engine = create_engine('file:data.db')
-
-
-def get_session():
-    with Session(engine) as session:
-        yield session
--- app/utils/StrictModel.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, ConfigDict
-
-
-class StrictModel(BaseModel):
-    model_config = ConfigDict(
-        frozen=True, str_strip_whitespace=True, strict=True
-    )
