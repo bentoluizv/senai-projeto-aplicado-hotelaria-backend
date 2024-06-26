3394177359b1efaf2daf47337e647b5da2424f66
Bento Luiz <bentoluizv@gmail.com>
2024-06-26 14:13:09 -0300

    add cors config

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import accommodation, booking, guests
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(guests.router)
 app.include_router(accommodation.router)
