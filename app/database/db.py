28f3bfa82e179f706d02bc6b84028f048ef8b684
Bento Luiz <bentoluizv@gmail.com>
2024-06-26 11:42:12 -0300

    add sqlalchemy session generator

---
+++ app/database/db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from app.database.models import Base
+
+engine = create_engine('sqlite:///./database.db')
+Base.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
