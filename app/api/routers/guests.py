5d1e1de11b1db6609ff3c092ce047f1d4144e57c
Bento Luiz <bentoluizv@gmail.com>
2024-06-26 11:45:00 -0300

    add guests api

---
+++ app/api/routers/guests.py
@@ -0,0 +1,98 @@
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.database.db import get_session
+from app.database.models import GuestDB
+from app.domain.Guest import Guest, GuestList
+
+router = APIRouter(tags=['Hóspedes'], prefix='/hospedes')
+
+
+@router.post(
+    '/',
+    status_code=HTTPStatus.CREATED,
+    response_model=Guest,
+)
+async def create_guest(guest: Guest, session: Session = Depends(get_session)):
+    db_guest = session.get(GuestDB, guest.document)
+
+    if db_guest:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail='Guest already exists'
+        )
+    db_guest = GuestDB(
+        name=guest.name,
+        document=guest.document,
+        created_at=guest.created_at.isoformat(),
+        surname=guest.surname,
+        country=guest.country,
+        phone=guest.phone,
+    )
+
+    session.add(db_guest)
+    session.commit()
+
+    return db_guest
+
+
+@router.get('/', status_code=HTTPStatus.OK, response_model=GuestList)
+async def list_all_guests(session: Session = Depends(get_session)):
+    db_guests = session.scalars(select(GuestDB)).all()
+    return {'guests': db_guests}
+
+
+@router.get(
+    '/{document}',
+    status_code=HTTPStatus.OK,
+    response_model=Guest,
+)
+async def find_guest(document: str, session: Session = Depends(get_session)):
+    db_guest = session.get(GuestDB, document)
+
+    if not db_guest:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Guest not found'
+        )
+
+    return db_guest
+
+
+@router.put(
+    '/{document}',
+    status_code=HTTPStatus.OK,
+    response_model=Guest,
+)
+async def update_guest(
+    document: str, guest: Guest, session: Session = Depends(get_session)
+):
+    db_guest = session.get(GuestDB, document)
+
+    if not db_guest:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Guest not found'
+        )
+    db_guest.name = guest.name
+    db_guest.surname = guest.surname
+    db_guest.document = guest.document
+    db_guest.phone = guest.phone
+    db_guest.country = guest.country
+
+    session.commit()
+    session.refresh(db_guest)
+
+    return db_guest
+
+
+@router.delete('/{document}', status_code=HTTPStatus.NO_CONTENT)
+async def delete_guest(document: str, session: Session = Depends(get_session)):
+    db_guest = session.get(GuestDB, document)
+
+    if not db_guest:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Guest not found'
+        )
+    session.delete(db_guest)
+    session.commit()
---
+++ tests/integration/test_api_guests.py
@@ -0,0 +1,51 @@
+from http import HTTPStatus
+
+
+def test_create_a_guest(client):
+    data = {
+        'document': '00157624242',
+        'name': 'Bento Luiz',
+        'surname': 'Machado',
+        'country': 'Brasil',
+        'phone': '48992054211',
+    }
+    response = client.post('/hospedes', json=data)
+    data = response.json()
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert 'created_at' in data
+
+
+def test_list_all_guests(client):
+    TOTAL_GUESTS = 5
+    response = client.get('/hospedes')
+    data = response.json()
+    assert response.status_code == HTTPStatus.OK
+    assert len(data['guests']) == TOTAL_GUESTS
+
+
+def test_find_guest(client):
+    response = client.get('/hospedes/98765432100')
+    data = response.json()
+    assert response.status_code == HTTPStatus.OK
+    assert data['name'] == 'Maria'
+
+
+def test_update_guest(client):
+    data = {
+        'document': '98765432100',
+        'name': 'Maria de Lurdes',
+        'surname': 'Machado',
+        'country': 'Brasil',
+        'phone': '489998765467',
+    }
+    response = client.put('/hospedes/98765432100', json=data)
+
+    data = response.json()
+    assert response.status_code == HTTPStatus.OK
+    assert data['name'] == 'Maria de Lurdes'
+
+
+def test_delete_guest(client):
+    response = client.delete('/hospedes/98765432100')
+    assert response.status_code == HTTPStatus.NO_CONTENT
