940c62bdc714c9ab5c4c0286dd0bdc4e36aada75
Bento Luiz <bentoluizv@gmail.com>
2024-06-27 17:37:50 -0300

    db now execute initial dump

--- app/database/db.py
+++ app/database/db.py
@@ -1,12 +1,26 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 
-from app.database.models import Base
+from app.database.db_init import db_init
+from app.database.models import Base, GuestDB
 
-engine = create_engine('sqlite:///./database.db')
+engine = create_engine('sqlite:///./database.db', echo=True)
+
 Base.metadata.create_all(engine)
+
+
+with Session(engine) as session:
+    existing_data = session.scalars(select(GuestDB)).all()
+
+    if len(existing_data) == 0:
+        db_init(session)
+        session.commit()
+        session.close()
 
 
 def get_session():
     with Session(engine) as session:
         yield session
+
+        session.commit()
+        session.close()
