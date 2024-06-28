d60de494d799933d629a5fe645b822bfa1f32ef9
Bento Luiz <bentoluizv@gmail.com>
2024-06-28 16:22:34 -0300

    remove status validation

--- app/domain/Booking.py
+++ app/domain/Booking.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import List
 from uuid import uuid4
 
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field
 
 from app.domain.Accommodation import Accommodation
 from app.domain.Guest import Guest
@@ -18,41 +18,6 @@ class Booking(BaseModel):
     guest: Guest
     accommodation: Accommodation
     budget: int
-
-    @model_validator(mode='after')
-    def update_status(self):
-        today = datetime.now()
-        is_in_check_in_period = today < self.check_in
-        is_in_booking_period = (
-            today > self.check_in and today <= self.check_out
-        )
-        is_in_check_out_period = today >= self.check_out
-
-        if is_in_check_in_period:
-            days_to_check_in = self.check_in - today
-            if days_to_check_in.days <= 1:
-                self.status = 'Aguardando Check In'
-                self.accommodation.status = 'Aguardando Reserva'
-
-        if is_in_booking_period:
-            if self.status == 'Ativa':
-                self.accommodation.status = 'Ocupada'
-
-            if self.status != 'Ativa':
-                self.status = 'Atrasada'
-
-        if is_in_check_out_period:
-            if self.status == 'Finalizada':
-                self.accommodation.status = 'Disponivel'
-
-            if self.status != 'Finalizada':
-                if self.check_out == today:
-                    self.status = 'Aguardando Checkout'
-
-                if today > self.check_out:
-                    self.status = 'Checkout Atrasado'
-
-        return self
 
     def set_status(self, status: str):
         self.status = status
