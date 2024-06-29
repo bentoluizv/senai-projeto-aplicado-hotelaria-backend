6192c0a8bf993b476d2e997a711c42b20b7c1670
Bento Luiz <bentoluizv@gmail.com>
2024-06-29 12:02:07 -0300

    set especific origins for cors

--- app/api/main.py
+++ app/api/main.py
@@ -6,9 +6,16 @@ from .routers import accommodation, amenities, booking, guests
 app = FastAPI()
 
 
+const = origins = [
+    'http://localhost',
+    'http://localhost:8080',
+    'http://127.0.0.1:4321/',
+    'http://127.0.0.1:8000/',
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
