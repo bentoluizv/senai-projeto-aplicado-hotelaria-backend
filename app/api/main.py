f1a81716dea95c897990d0764d7eb00289255a90
Bento Luiz <bentoluizv@gmail.com>
2024-06-30 10:13:13 -0300

    change cors policy

--- app/api/main.py
+++ app/api/main.py
@@ -6,16 +6,9 @@ from .routers import accommodation, amenities, booking, guests
 app = FastAPI()
 
 
-const = origins = [
-    'http://localhost',
-    'http://localhost:8080',
-    'http://127.0.0.1:4321/',
-    'http://127.0.0.1:8000/',
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
