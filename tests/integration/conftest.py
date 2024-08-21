3f39d8410d814f919d48043e1ca995bb996c7c0f
Bento Luiz <bentoluizv@gmail.com>
2024-08-21 10:42:59 -0300

    add new fixture for db engine with session scope.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -5,34 +5,31 @@ from sqlalchemy.orm import Session
 from sqlalchemy.pool import StaticPool
 
 from app.api.main import app
-from app.database.db import get_database_session
-from app.database.db_init import db_init
 from app.database.models import Base
 
 
-@pytest.fixture()
-def session():
+@pytest.fixture(scope='session')
+def engine():
     engine = create_engine(
         'sqlite+pysqlite:///:memory:',
         connect_args={'check_same_thread': False},
         poolclass=StaticPool,
     )
+
+    return engine
+
+
+@pytest.fixture()
+def session(engine):
     Base.metadata.create_all(engine)
 
     with Session(engine) as session:
-        db_init(session)
         yield session
 
     Base.metadata.drop_all(engine)
 
 
 @pytest.fixture()
-def client(session):
-    def get_session_override():
-        return session
-
+def client():
     with TestClient(app) as client:
-        app.dependency_overrides[get_database_session] = get_session_override
         yield client
-
-    app.dependency_overrides.clear()
