c4fbe745145f507b19c6e264734173892c885eac
Bento Luiz <bentoluizv@gmail.com>
2024-08-23 11:00:21 -0300

    testing db engine and session connection.

---
+++ tests/integration/test_db.py
@@ -0,0 +1,26 @@
+from sqlalchemy import select
+
+from app.database.models import UserDB
+
+
+def test_db_engine(engine):
+    assert engine.driver == 'pysqlite'
+    assert engine.name == 'sqlite'
+
+
+def test_db_session(session):
+    users = session.scalars(select(UserDB)).all()
+    assert len(users) == 1
+
+    user = session.scalar(
+        select(UserDB).where(UserDB.email == 'teste@teste.com')
+    )
+
+    assert user
+
+    session.delete(user)
+    session.commit()
+
+    _user = session.get(UserDB, user.uuid)
+
+    assert not _user
