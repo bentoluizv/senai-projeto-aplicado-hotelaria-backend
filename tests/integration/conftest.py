5047ce13fab32e20641e33ca7b0fc803c9bf5c80
Bento Luiz <bentoluizv@gmail.com>
2024-08-23 10:59:55 -0300

    now databse creates a single test user for each tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
@@ -5,7 +7,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.pool import StaticPool
 
 from app.api.main import app
-from app.database.models import Base
+from app.database.models import Base, UserDB
 
 
 @pytest.fixture(scope='session')
@@ -24,6 +26,13 @@ def session(engine):
     Base.metadata.create_all(engine)
 
     with Session(engine) as session:
+        new_user = UserDB(
+            uuid=uuid4(),
+            email='teste@teste.com',
+            password='superhardpassword',
+        )
+        session.add(new_user)
+
         yield session
 
     Base.metadata.drop_all(engine)
