6aa59c58631f2e9009ce16d23691f1c4a173e405
Bento Luiz <bentoluizv@gmail.com>
2024-08-25 15:49:26 -0300

    add the error message to itself

--- app/domain/errors/NotFoundError.py
+++ app/domain/errors/NotFoundError.py
@@ -2,3 +2,4 @@ class NotFoundError(Exception):
     def __init__(self, id):
         message = f'Resource with id "{id}" not found!'
         super().__init__(message)
+        self.message = message
