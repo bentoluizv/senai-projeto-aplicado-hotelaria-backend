0377138271cc430de5a8a5d23cc96538db31e844
Bento Luiz <bentoluizv@gmail.com>
2024-08-28 11:16:36 -0300

    remove min_nights parameter / add model validation to convert amenitie data.

--- app/domain/Accommodation.py
+++ app/domain/Accommodation.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+from typing import Any
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
-from app.data.database.models import AccommodationDB
+from app.data.database.models import AccommodationDB, AmenitieDB
 from app.domain.Amenitie import Amenitie
 
 
@@ -12,7 +13,6 @@ class AccommodationUpdateDTO(BaseModel):
     total_guests: int | None = None
     single_beds: int | None = None
     double_beds: int | None = None
-    min_nights: int | None = None
     price: float | None = None
     amenities: list[str] | None = None
 
@@ -23,13 +23,12 @@ class AccommodationCreateDTO(BaseModel):
     total_guests: int
     single_beds: int
     double_beds: int
-    min_nights: int
     price: float
     amenities: list[str]
 
 
 class Accommodation(BaseModel):
-    id: int
+    id: int | None = None
     created_at: datetime = Field(default_factory=datetime.now)
     name: str
     status: str
@@ -38,6 +37,21 @@ class Accommodation(BaseModel):
     double_beds: int
     price: float
     amenities: list[Amenitie]
+
+    @model_validator(mode='before')
+    @classmethod
+    def check_input_data(cls, data: Any) -> Any:
+        if all(isinstance(item, str) for item in data['amenities']):
+            data['amenities'] = [
+                Amenitie(name=amenitie) for amenitie in data['amenities']
+            ]
+
+        if all(isinstance(item, AmenitieDB) for item in data['amenities']):
+            data['amenities'] = [
+                Amenitie(name=amenitie.name, id=amenitie.id)
+                for amenitie in data['amenities']
+            ]
+        return data
 
     @classmethod
     def from_database(cls, data: AccommodationDB):
