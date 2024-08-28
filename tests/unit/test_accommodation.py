526de4115046353408bea336fa98537f1facfe4d
Bento Luiz <bentoluizv@gmail.com>
2024-08-28 11:22:34 -0300

    add accommodation unit test

---
+++ tests/unit/test_accommodation.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from app.data.database.models import AccommodationDB, AmenitieDB
+from app.domain.Accommodation import Accommodation, AccommodationCreateDTO
+
+
+def test_should_create_a_new_accommodation_from_dto():
+    accommodation_dto = AccommodationCreateDTO(
+        status='Disponivel',
+        amenities=['ar-condicionado'],
+        double_beds=2,
+        price=250,
+        single_beds=1,
+        total_guests=3,
+        name='Quarto de Testes',
+    )
+
+    accommodation = Accommodation.create(accommodation_dto)
+
+    assert accommodation
+    assert hasattr(accommodation, 'id')
+    assert hasattr(accommodation, 'created_at')
+
+
+def test_should_create_a_new_accommodation_from_db_obj():
+    guest_db = AccommodationDB(
+        created_at=datetime.now(),
+        status='Disponivel',
+        amenities=[AmenitieDB(name='WIFI')],
+        double_beds=2,
+        price=250,
+        single_beds=1,
+        total_guests=3,
+        name='Quarto de Testes',
+    )
+
+    accommodation = Accommodation.from_database(guest_db)
+
+    assert accommodation.name == 'Quarto de Testes'
+    assert hasattr(accommodation, 'id')
+    assert hasattr(accommodation, 'created_at')
