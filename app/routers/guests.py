a1a51f2f232cdad7567e883a308b1464f38a5967
Bento Luiz <bentoluizv@gmail.com>
2024-08-29 11:01:17 -0300

    remove repositories

--- app/routers/guests.py
+++ app/routers/guests.py
@@ -2,14 +2,11 @@ from http import HTTPStatus
 from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from app.data.database.db import get_database_session
-from app.data.repositories.GuestRepository import GuestRepository
-from app.domain.errors.AlreadyExistsError import AlreadyExistsError
-from app.domain.errors.NotFoundError import NotFoundError
 from app.domain.Guest import Guest, GuestCreateDTO, GuestUpdateDTO
 
 router = APIRouter(tags=['Hóspedes'], prefix='/guests')
@@ -22,26 +19,14 @@ class Message(BaseModel):
 @router.get('/', status_code=HTTPStatus.OK, response_model=list[Guest])
 async def list_all_guests(
     session: Annotated[Session, Depends(get_database_session)],
-) -> list[Guest]:
-    repository = GuestRepository(session)
-    guests = repository.list()
-    return guests
+) -> list[Guest]: ...
 
 
 @router.post('/', status_code=HTTPStatus.CREATED, response_model=Message)
 async def create_guest(
     data: GuestCreateDTO,
     session: Annotated[Session, Depends(get_database_session)],
-) -> Message:
-    try:
-        repository = GuestRepository(session)
-        repository.insert(data)
-        return Message(content='CREATED')
-
-    except AlreadyExistsError as error:
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT, detail=error.message
-        )
+) -> Message: ...
 
 
 @router.get(
@@ -51,16 +36,7 @@ async def create_guest(
 )
 async def find_guest(
     uuid: UUID, session: Annotated[Session, Depends(get_database_session)]
-) -> Guest:
-    try:
-        repository = GuestRepository(session)
-        guest = repository.find_by_id(uuid)
-        return guest
-
-    except NotFoundError as error:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail=error.message
-        )
+) -> Guest: ...
 
 
 @router.put(
@@ -72,28 +48,10 @@ async def update_guest(
     uuid: UUID,
     data: GuestUpdateDTO,
     session: Annotated[Session, Depends(get_database_session)],
-):
-    try:
-        repository = GuestRepository(session)
-        repository.update(uuid, data)
-        return Message(content='UPDATED')
-
-    except NotFoundError as error:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail=error.message
-        )
+): ...
 
 
 @router.delete('/{uuid}', status_code=HTTPStatus.OK, response_model=Message)
 async def delete_guest(
     uuid: UUID, session: Annotated[Session, Depends(get_database_session)]
-):
-    try:
-        repository = GuestRepository(session)
-        repository.delete(uuid)
-        return Message(content='DELETED')
-
-    except NotFoundError as error:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail=error.message
-        )
+): ...
