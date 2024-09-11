6f231e491de2df022ef3a9a486b60966483a02aa
bentoluizv <bentoluizv@gmail.com>
2024-09-11 09:50:42 -0300

    add supabase connection url

--- app/infra/database/db.py
+++ app/infra/database/db.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from app.infra.database.models import Base
+from app.settings.Settings import Settings
 
-engine = create_engine('sqlite:///./database.db')
+engine = create_engine(
+    f'postgresql://postgres.ourkxxhmvdlnpkghomcn:{Settings().DATABASE_PASSWORD}@aws-0-sa-east-1.pooler.supabase.com:6543/postgres'  # type: ignore
+)
 
 Base.metadata.create_all(engine)
 
