59fa693331c2c7277452eebebf45861a72870849
bentoluizv <bentoluizv@gmail.com>
2024-10-13 20:49:04 -0300

    mv status class to models

--- app/database/models.py
+++ app/database/models.py
@@ -1,3 +1,4 @@
+import enum
 from datetime import datetime
 
 from pydantic import EmailStr
@@ -19,9 +20,25 @@ from sqlalchemy.orm import (
     relationship,
 )
 
-from app.schemas.Accommodation import Status as AccommodationStatus
-from app.schemas.Booking import Status as BookingStatus
-from app.schemas.User import Role
+
+class BookingStatus(enum.Enum):
+    BOOKED = 'booked'
+    WAITING_CHECK_IN = 'waiting check in'
+    ACTIVE = 'active'
+    WAITING_CHECK_OUT = 'waiting check out'
+    COMPLETED = 'completed'
+    CANCELED = 'canceled'
+
+
+class AccommodationStatus(enum.Enum):
+    AVAIABLE = 'avaiable'
+    OCUPIED = 'ocupied'
+
+
+class Role(enum.Enum):
+    ADMIN = 'admin'
+    USER = 'user'
+    GUEST = 'guest'
 
 
 class Base(MappedAsDataclass, DeclarativeBase):
