b0badc2dfced0dc8c8778ef693feec9c7cdb7284
bentoluizv <bentoluizv@gmail.com>
2024-10-14 09:40:15 -0300

    add bookings /GET/id

--- app/api/bookings.py
+++ app/api/bookings.py
@@ -13,6 +13,8 @@ router = APIRouter(
     dependencies=[],
 )
 
+BookingController = Annotated[BookingController, Depends(BookingController)]
+
 
 class BookingList(BaseModel):
     bookings: list[Booking]
@@ -20,11 +22,15 @@ class BookingList(BaseModel):
 
 @router.get('/', response_model=BookingList, status_code=HTTPStatus.OK)
 def list_all_bookings(
-    booking_controller: Annotated[
-        BookingController, Depends(BookingController)
-    ],
+    booking_controller: BookingController,  # type: ignore
     page: int = 1,
     per_page: int = 10,
 ):
     bookings = booking_controller.list_all(page=page, per_page=per_page)
     return BookingList(bookings=bookings)
+
+
+@router.get('/{id}', status_code=HTTPStatus.OK, response_model=Booking)
+def find_by_id(booking_controller: BookingController, id: str):  # type: ignore
+    booking = booking_controller.find_by_id(id)
+    return booking
