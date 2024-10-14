8aefb5ffc28d05a1e499d67cde4d9ba5bc4e1105
bentoluizv <bentoluizv@gmail.com>
2024-10-14 09:40:06 -0300

    add api tests for bookings /GET/id

--- tests/integration/api/test_bookings.py
+++ tests/integration/api/test_bookings.py
@@ -1,9 +1,11 @@
 from http import HTTPStatus
 
+import pytest
+
 
 def test_list_all_bookings(client):
     TOTAL_BOOKINGS = 10
-    response = client.get('/bookings/?page=1&per_page=10')
+    response = client.get('/bookings/')
     assert response.status_code == HTTPStatus.OK
     assert isinstance(response.json(), dict)
     assert 'bookings' in response.json()
@@ -28,3 +30,15 @@ def test_list_all_out_range(client):
         response.json()['detail']
         == 'Page 8 is out of range. There are only 6 pages.'
     )
+
+
+@pytest.mark.usefixtures('_db_booking')
+def test_find_booking_by_id(client):
+    response = client.get('/bookings/01JA5EZ0BBQRGDX69PNTVG3N5E')
+    booking = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert isinstance(response.json(), dict)
+    assert booking['guest']
+    assert booking['accommodation']
+    assert booking['budget'] > 0
