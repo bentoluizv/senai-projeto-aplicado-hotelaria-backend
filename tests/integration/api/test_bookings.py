5f0e1da0b8774322cf09ace5a757edc7bc6cdaed
bentoluizv <bentoluizv@gmail.com>
2024-10-14 09:00:10 -0300

    fix api list all tests to check out of range pagination

--- tests/integration/api/test_bookings.py
+++ tests/integration/api/test_bookings.py
@@ -20,8 +20,11 @@ def test_list_all_bookings_20_per_page(client):
 
 
 def test_list_all_out_range(client):
-    response = client.get('/bookings/?page=5&per_page=20')
-    assert response.status_code == HTTPStatus.OK
+    response = client.get('/bookings/?page=8&per_page=5')
+    assert response.status_code == HTTPStatus.BAD_REQUEST
     assert isinstance(response.json(), dict)
-    assert 'bookings' in response.json()
-    assert len(response.json()['bookings']) == 0
+    assert 'detail' in response.json()
+    assert (
+        response.json()['detail']
+        == 'Page 8 is out of range. There are only 6 pages.'
+    )
