0c130993e99c980e0475dbbec4be71f1a424d916
bentoluizv <bentoluizv@gmail.com>
2024-10-14 08:44:39 -0300

    add route to app

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from .api import bookings
+
 app = FastAPI()
 
 
@@ -12,3 +14,5 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+app.include_router(bookings.router)
