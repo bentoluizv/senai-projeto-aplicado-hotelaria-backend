30cd23175053d457807734b3f05b46c0382c34d7
bentoluizv <bentoluizv@gmail.com>
2024-10-14 09:39:29 -0300

    add test for repository find by id

--- tests/integration/database/repository/test_booking_repository.py
+++ tests/integration/database/repository/test_booking_repository.py
@@ -23,3 +23,9 @@ def test_list_all_out_range_return_0(booking_repository):
     TOTAL_BOOKINGS = 0
     bookings = booking_repository.list_all(page=60, per_page=5)
     assert len(bookings) == TOTAL_BOOKINGS
+
+
+@pytest.mark.usefixtures('_db_booking')
+def test_find_booking_by_id(booking_repository):
+    booking = booking_repository.find_by_id('01JA5EZ0BBQRGDX69PNTVG3N5E')
+    assert booking
