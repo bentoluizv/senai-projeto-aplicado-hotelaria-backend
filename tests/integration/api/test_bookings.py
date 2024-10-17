62d48074c9df96d2ae3580e5b2604702b955b823
bentoluizv <bentoluizv@gmail.com>
2024-10-17 10:52:19 -0300

    add parametrized conflict checks

--- tests/integration/api/test_bookings.py
+++ tests/integration/api/test_bookings.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from http import HTTPStatus
 
+import pytest
+
 from app.entities.Booking import BookingCreateDTO, BookingUpdateDTO
 
 
@@ -34,7 +36,7 @@ def test_list_all_out_range(client):
 
 
 def test_find_booking_by_id(client, db_booking):
-    response = client.get('/bookings/01JA5EZ0BBQRGDX69PNTVG3N5E')
+    response = client.get(f'/bookings/{db_booking.ulid}')
     booking = response.json()
 
     assert response.status_code == HTTPStatus.OK
@@ -56,17 +58,67 @@ def test_not_found_booking_by_id(client):
     )
 
 
-def test_create_new_booking(client, db_guest, db_accommodation):
+@pytest.mark.parametrize(
+    ('check_in', 'check_out', 'expected_status'),
+    [
+        # Testes sem conflito
+        (
+            datetime(2023, 5, 11),
+            datetime(2023, 5, 20),
+            HTTPStatus.CREATED,
+        ),  # Fora do período da primeira reserva
+        (
+            datetime(2023, 6, 21),
+            datetime(2023, 6, 25),
+            HTTPStatus.CREATED,
+        ),  # Fora do período da segunda reserva
+        (
+            datetime(2024, 12, 16),
+            datetime(2024, 12, 20),
+            HTTPStatus.CREATED,
+        ),  # Fora do período da quinta reserva
+        # Testes com conflito completo
+        (
+            datetime(2023, 5, 2),
+            datetime(2023, 5, 5),
+            HTTPStatus.CONFLICT,
+        ),  # Dentro da primeira reserva
+        (
+            datetime(2024, 12, 2),
+            datetime(2024, 12, 14),
+            HTTPStatus.CONFLICT,
+        ),  # Dentro da quinta reserva
+        # Testes com conflito parcial
+        (
+            datetime(2023, 5, 9),
+            datetime(2023, 5, 15),
+            HTTPStatus.CONFLICT,
+        ),  # Sobreposição no final da primeira reserva
+        (
+            datetime(2023, 6, 14),
+            datetime(2023, 6, 16),
+            HTTPStatus.CONFLICT,
+        ),  # Sobreposição no início da segunda reserva
+        (
+            datetime(2024, 12, 10),
+            datetime(2024, 12, 20),
+            HTTPStatus.CONFLICT,
+        ),  # Sobreposição no final da quinta reserva
+    ],
+)
+def test_create_new_booking(  # noqa: PLR0913, PLR0917
+    client, db_guest, db_accommodation, check_in, check_out, expected_status
+):
     dto = BookingCreateDTO(
-        check_in=datetime(2026, 1, 1),
-        check_out=datetime(2026, 1, 12),
-        guest_document='2672713987',
-        accommodation_ulid='01JA5EZ0BBQRGDX69PNTVG3N5E',
+        check_in=check_in,
+        check_out=check_out,
+        guest_document=db_guest.document,
+        accommodation_ulid=db_accommodation.ulid,
     )
 
     data = dto.model_dump_json()
     response = client.post('/bookings', data=data)
-    assert response.status_code == HTTPStatus.CREATED
+    assert response.status_code == expected_status
 
 
 def test_update_booking(client, db_booking):
@@ -76,10 +128,26 @@ def test_update_booking(client, db_booking):
     )
 
     data = dto.model_dump_json()
-    response = client.put('/bookings/01JA5EZ0BBQRGDX69PNTVG3N5E', data=data)
+    response = client.put(f'/bookings/{db_booking.ulid}', data=data)
     assert response.status_code == HTTPStatus.OK
+
+
+def test_update_non_existent_booking(client):
+    dto = BookingUpdateDTO(
+        check_in=datetime(2026, 1, 1),
+        check_out=datetime(2026, 1, 12),
+    )
+
+    data = dto.model_dump_json()
+    response = client.put('/bookings/01JA5EZ0BBQRGDX69PNTVG3N5E', data=data)
+    assert response.status_code == HTTPStatus.NOT_FOUND
 
 
 def test_delete_booking(client, db_booking):
-    response = client.delete('/bookings/01JA5EZ0BBQRGDX69PNTVG3N5E')
+    response = client.delete(f'/bookings/{db_booking.ulid}')
     assert response.status_code == HTTPStatus.OK
+
+
+def test_delete_non_existent_booking(client):
+    response = client.delete('/bookings/01JA5EZ0BBQRGDX69PNTVG3N5E')
+    assert response.status_code == HTTPStatus.NOT_FOUND
