50e81295157f3c36f82736354152e2a765b40577
bentoluizv <bentoluizv@gmail.com>
2024-10-17 09:32:03 -0300

    add all repositories to factory method

--- app/factory/RepositoryFactory.py
+++ app/factory/RepositoryFactory.py
@@ -4,7 +4,12 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.database.db import get_database_session
+from app.database.repositories.AccommodationRepository import (
+    AccommodationRepository,
+)
 from app.database.repositories.BookingRepository import BookingRepository
+from app.database.repositories.GuestRepository import GuestRepository
+from app.database.repositories.UserRepository import UserRepository
 
 
 class RepositoryFactory:
@@ -17,3 +22,12 @@ class RepositoryFactory:
 
     def create_booking_respository(self):
         return BookingRepository(self.session)
+
+    def create_guest_respository(self):
+        return GuestRepository(self.session)
+
+    def create_accommodation_respository(self):
+        return AccommodationRepository(self.session)
+
+    def create_user_respository(self):
+        return UserRepository(self.session)
