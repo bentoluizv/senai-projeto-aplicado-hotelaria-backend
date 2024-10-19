8e7c323e1dbaafd42ba94713973c437f7b4c1efc
bentoluizv <bentoluizv@gmail.com>
2024-10-19 17:17:32 -0300

    add user repository test suite

---
+++ tests/integration/database/test_user_repository.py
@@ -0,0 +1,40 @@
+import pytest
+from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
+
+from app.database.models import Role, UserDB
+from app.entities.User import User, UserCreateDTO
+
+
+@pytest.fixture()
+def user_repository(repository_factory):
+    user_repository = repository_factory.create_user_respository()
+    return user_repository
+
+
+def test_not_found_user_by_name(user_repository):
+    with pytest.raises(NoResultFound):
+        user_repository.find_by_email('user@user.com')
+
+
+def test_find_user_by_email(user_repository, db_user):
+    user = user_repository.find_by_email('bentoluizv@gmail.com')
+    assert user
+
+
+def test_create_user(user_repository, session):
+    dto = UserCreateDTO(
+        email='bentoluizv@gmail.com',
+        password='12334',
+        password2='12334',
+        role=Role.ADMIN,
+    )
+    user = User.create(dto)
+
+    user_repository.create(user)
+
+    user_created = session.scalar(
+        select(UserDB).where(UserDB.email == user.email)
+    )
+    assert user_created is not None
+    assert user.password.startswith('$argon2')
