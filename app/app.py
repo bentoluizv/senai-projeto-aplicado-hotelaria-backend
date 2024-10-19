9671d4cfd50e74458d0627ddd12681d9fceb3934
bentoluizv <bentoluizv@gmail.com>
2024-10-19 19:58:32 -0300

    add routers to app

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .api import bookings
+from .api import accommodations, amenities, bookings, guests
 
 app = FastAPI()
 
@@ -16,3 +16,6 @@ app.add_middleware(
 )
 
 app.include_router(bookings.router)
+app.include_router(amenities.router)
+app.include_router(accommodations.router)
+app.include_router(guests.router)
