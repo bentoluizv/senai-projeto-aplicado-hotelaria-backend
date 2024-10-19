cd0f24106c4e6ca69e7672a2c87c18a0fc1e0e8d
bentoluizv <bentoluizv@gmail.com>
2024-10-19 17:16:18 -0300

    fix user repository

--- app/database/repositories/UserRepository.py
+++ app/database/repositories/UserRepository.py
@@ -1,9 +1,10 @@
 from pydantic import EmailStr
 from sqlalchemy import func, select
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session
 
-from app.database.models import GuestDB, UserDB
-from app.entities.User import User, UserCreateDTO
+from app.database.models import UserDB
+from app.entities.User import User
 
 
 class UserRepository:
@@ -14,21 +15,12 @@ class UserRepository:
 
     def count(self) -> int:
         total_accommodation = self.session.scalar(
-            select(func.count()).select_from(GuestDB)
+            select(func.count()).select_from(UserDB)
         )
 
         return total_accommodation or 0
 
-    def create(self, dto: UserCreateDTO) -> None:
-        db_user = self.session.scalar(
-            select(UserDB).where(UserDB.email == dto.email)
-        )
-
-        if db_user:
-            return None
-
-        user = User.create(dto)
-
+    def create(self, user: User) -> None:
         db_user = UserDB(
             ulid=str(user.ulid),
             email=user.email,
@@ -36,7 +28,7 @@ class UserRepository:
             role=user.role,
         )
 
-        self.session.add(user)
+        self.session.add(db_user)
         self.session.commit()
 
     def find_by_email(self, email: EmailStr) -> User | None:
@@ -45,7 +37,7 @@ class UserRepository:
         )
 
         if not db_user:
-            return None
+            raise NoResultFound()
 
         user = User.from_db(db_user)
 
