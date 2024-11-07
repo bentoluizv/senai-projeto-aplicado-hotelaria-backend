f5426d98d0ad98f2cdef2882979deacbb6b7aa09
bentoluizv <bentoluizv@gmail.com>
2024-11-07 10:52:09 -0300

    add guest schema

--- frontend/portal_hospede/src/schemas/types.ts
+++ frontend/portal_hospede/src/schemas/types.ts
@@ -16,26 +16,51 @@ export const AccommodationSchema = z.object({
   amenities: z.array(AmenitieSchema),
 });
 
+export const AccommodationListSchema = z.array(AccommodationSchema);
+
 export type Accommodation = z.infer<typeof AccommodationSchema>;
 
 export type Amenitie = z.infer<typeof AccommodationSchema>;
 
 export const BookingSearchParamSchema = z
   .object({
-    checkin: z.string().date("A data de checkin deve ser uma data válida"),
-    checkout: z.string().date("A data de checkout deve ser uma data válida"),
+    checkin: z
+      .string()
+      .date("A data de checkin deve ser uma data válida")
+      .transform((value) => new Date(value)),
+    checkout: z
+      .string()
+      .date("A data de checkout deve ser uma data válida")
+      .transform((value) => new Date(value)),
   })
-  .refine(
-    (schema) => {
-      const checkIn = new Date(schema.checkin);
-      const checkOut = new Date(schema.checkout);
-
-      return checkOut > checkIn;
-    },
-    {
-      message: "A data de checkout deve ser posterior à data de checkin",
-      path: ["checkout"],
-    }
-  );
+  .refine(({ checkin, checkout }) => checkout > checkin, {
+    message: "A data de checkout deve ser posterior à data de checkin",
+    path: ["checkout"],
+  });
 
 export type BookingSearchParam = z.infer<typeof BookingSearchParamSchema>;
+
+export const guestSchema = z.object({
+  ulid: z.string().ulid(),
+  name: z.string(),
+  surname: z.string(),
+  phone: z.string(),
+  country: z.string(),
+});
+
+export type Guest = z.infer<typeof guestSchema>;
+
+export const bookingSchema = z.object({
+  ulid: z.string().ulid(),
+  status: z.string(),
+  locator: z.string(),
+  check_in: z.string().transform((value) => new Date(value)),
+  check_out: z.string().transform((value) => new Date(value)),
+  budget: z.number().nonnegative(),
+  guest: guestSchema,
+  accommodation: AccommodationSchema,
+});
+
+export type Booking = z.infer<typeof bookingSchema>;
+
+export const bookingListSchema = z.array(bookingSchema);
